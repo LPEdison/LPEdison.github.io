5032284352fc8780d94ed4f32b5ab05209b97373
LPEdison <github.edison@gmx.com>
2024-12-13 14:30:43 +0100

    Webstudio Migration
    removed nicepage and migrated to webstudio, not complete, might be broken

---
+++ assets/entries/pages_copy-1_copy-1.DQ_u5qVd.js
@@ -0,0 +1,143 @@
+import{j as e,f as w,a as s,b as h,p as c,O as x,x as b,n as f,M as j,R as y,N as k,S as v,s as a,$ as S,P as _,c as d,i as m,d as g,e as z,g as N,h as P}from"../chunks/chunk-C-sEj5Lc.js";import{f as r,l as q}from"../chunks/chunk-BE_mumB7.js";/* empty css                      */const p=void 0,T=void 0,C=[],I=[],O=({})=>e.jsxs(w,{className:"w-body cef8lky c3i1u3x chk6qok",children:[e.jsxs(s,{className:"w-box cxit5en cogsgih ctdzyps ce19egy",children:[e.jsx(h,{href:"/",className:"w-link",children:e.jsx(c,{src:"/assets/Logo_fgtINaQGBFFapgXNvmtgD.png",width:405,height:451,className:"w-image c942crn c1uzvt68 cp3a1ni"})}),e.jsxs(x,{children:[e.jsx(b,{children:e.jsx(f,{className:"w-button c1uqrecz c10ua9p1 c1qy06sr cpbg552 cshxx6g c1lub6ji c77fen c1kt3hjx c1geiwv ckcg2od cealztw cvs4hrp comp0x6 cqpuunw c1wvok5d c1eikf4o c1iwr2ut c1l088 c1fh3e63 c14zbtl7 c14xknmu ci4hcet c1xm82re caz9sw9 c3ke8ei c1vra1wj c1rkfrji c1t36fdn c1twcrqe",children:e.jsx(c,{src:"/assets/burger_H_E62dUjUdnjYrHNINiU_.png",width:100,height:100,className:"w-image"})})}),e.jsx(j,{className:"w-dialog-overlay c1n92csm c1e0yeox cpjqu7u csvz69a c62v901 cnpumd1 c6gisag c1yxak60 cbe9vyj cqsyioe c13jgw8t ccvhcq8",children:e.jsxs(y,{className:"w-dialog-content c1s06m5q cnpumd1 cbe9vyj cqsyioe c15zk8e1 cb947cc c6gisag ckwfs5l c14zbtl7 c13gmqhk cpg64ub cxu11uu ci1jhro c1xyla5v",children:[e.jsxs(s,{tag:"nav",role:"navigation",className:"w-box",children:[e.jsxs(s,{className:"w-box cbe9vyj cqsyioe c1p8wosj c1wpxkon",children:[e.jsx(k,{className:"w-dialog-title c19otp7c c14dh9mz cmup7a7 cbyfgle cwvbj7b",children:"Sheet Title"}),e.jsx(v,{className:"w-dialog-description c19otp7c c14dh9mz c1geiwv ckcg2od c8wnu40",children:"Sheet description text you can edit"})]}),e.jsx(a,{className:"w-text",children:"The text you can edit"})]}),e.jsx(S,{className:"w-close-button c193n3ab c11gcw2s ci9xck c164arb c1mydgbc chzyyxd c1rkdclf cspx8cz cbe9vyj c1qy06sr cpbg552 c17x55us cpbtfu3 c1uqrecz c350vec cwwj380 ci4hcet c2ls53r cjhbkuo",children:e.jsx(_,{code:'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" width="100%" height="100%" style="display: block;"><path fill-rule="evenodd" d="M13.566 2.434a.8.8 0 0 1 0 1.132L9.13 8l4.435 4.434a.8.8 0 0 1-1.132 1.132L8 9.13l-4.434 4.435a.8.8 0 0 1-1.132-1.132L6.87 8 2.434 3.566a.8.8 0 0 1 1.132-1.132L8 6.87l4.434-4.435a.8.8 0 0 1 1.132 0Z" clip-rule="evenodd"/></svg>',className:"w-html-embed"})})]})})]})]}),e.jsxs(s,{className:"w-box",children:[e.jsx(a,{className:"w-text chewl0k cs8jnol c1bb95j4 c136d2hq",children:"Keep in mind these resistor colors!"}),e.jsxs(s,{className:"w-box c1h1nl5c c13gmqhk ckfm5zf c1f1okbg ch1fyv9 cwo00xu cotpzvh ccwx88h c11693zx co8eotk",children:[e.jsx(c,{src:"/assets/Capture_d__cran_2024-12-03_160109_PJ2UudRaJD8_bza_X_OKG.png",width:389,height:147,className:"w-image c1v19npp c13gmqhk c1q9b457 cgy3e4m"}),e.jsx(c,{src:"/assets/Capture_d__cran_2024-12-03_160212_eOl_PoBJ8HHeWHs-g4RPi.png",width:390,height:152,className:"w-image c1h1nl5c c19otp7c c14dh9mz c1st55tw c3793j1 c1uzvt68 cw8wk4g c1vcoo9v c3j6eu6 c1xui7l2"}),e.jsx(c,{src:"/assets/Capture_d__cran_2024-12-03_155524_dbw-G-XKY1jhwxOwFXklI.png",width:396,height:149,className:"w-image c1h1nl5c c13gmqhk c1d5e9u5 c3793j1 c14zbtl7"})]})]}),e.jsxs(s,{className:"w-box",children:[e.jsx(a,{className:"w-text c1jsn5zb camnv7y c1j6gywh",children:e.jsx(a,{tag:"span",className:"w-text",children:e.jsx(d,{className:"w-bold-text c1c7894k",children:"RGB Server"})})}),e.jsxs(r,{className:"w-paragraph ck3rk9r cz5s00m cgzpa1h c1g05gyx",children:["In this page we'll learn how to connect our ESP-32 to a WI-FI network and setting up a simple premade server to control an RGB light. Before proceeding make sure you finishe the getting Started page!","",e.jsx("br",{}),"","",e.jsx("br",{}),"","Here is the code we will be using for this part"]}),e.jsx(q,{code:`// Load Wi-Fi library
+#include <WiFi.h>
+
+// Replace with your network credentials
+const char* ssid = "YOURSSID";
+const char* password = "YOURPASSWORD";
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Decode HTTP GET value
+String redString = "0";
+String greenString = "0";
+String blueString = "0";
+int pos1 = 0;
+int pos2 = 0;
+int pos3 = 0;
+int pos4 = 0;
+
+// Variable to store the HTTP req  uest
+String header;
+
+// Red, green, and blue pins for PWM control
+const int redPin = 13;     // 13 corresponds to GPIO13
+const int greenPin = 12;   // 12 corresponds to GPIO12
+const int bluePin = 14;    // 14 corresponds to GPIO14
+
+// Setting PWM frequency, channels and bit resolution
+const int freq = 5000;
+const int redChannel = 0;
+const int greenChannel = 1;
+const int blueChannel = 2;
+// Bit resolution 2^8 = 256
+const int resolution = 8;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0;
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
+
+void setup() {
+  Serial.begin(115200);
+  // configure LED PWM functionalitites
+  ledcSetup(redChannel, freq, resolution);
+  ledcSetup(greenChannel, freq, resolution);
+  ledcSetup(blueChannel, freq, resolution);
+ 
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(redPin, redChannel);
+  ledcAttachPin(greenPin, greenChannel);
+  ledcAttachPin(bluePin, blueChannel);
+ 
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {            // loop while the client's connected
+      currentTime = millis();
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+                   
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\\"viewport\\" content=\\"width=device-width, initial-scale=1\\">");
+            client.println("<link rel=\\"icon\\" href=\\"data:,\\">");
+            client.println("<link rel=\\"stylesheet\\" href=\\"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css\\">");
+            client.println("<script src=\\"https://cdnjs.cloudflare.com/ajax/libs/jscolor/2.0.4/jscolor.min.js\\"><\/script>");
+            client.println("</head><body><div class=\\"container\\"><div class=\\"row\\"><h1>ESP Color Picker</h1></div>");
+            client.println("<a class=\\"btn btn-primary btn-lg\\" href=\\"#\\" id=\\"change_color\\" role=\\"button\\">Change Color</a> ");
+            client.println("<input class=\\"jscolor {onFineChange:'update(this)'}\\" id=\\"rgb\\"></div>");
+            client.println("<script>function update(picker) {document.getElementById('rgb').innerHTML = Math.round(picker.rgb[0]) + ', ' +  Math.round(picker.rgb[1]) + ', ' + Math.round(picker.rgb[2]);");
+            client.println("document.getElementById(\\"change_color\\").href=\\"?r\\" + Math.round(picker.rgb[0]) + \\"g\\" +  Math.round(picker.rgb[1]) + \\"b\\" + Math.round(picker.rgb[2]) + \\"&\\";}<\/script></body></html>");
+            // The HTTP response ends with another blank line
+            client.println();
+
+            // Request sample: /?r201g32b255&
+            // Red = 201 | Green = 32 | Blue = 255
+            if(header.indexOf("GET /?r") >= 0) {
+              pos1 = header.indexOf('r');
+              pos2 = header.indexOf('g');
+              pos3 = header.indexOf('b');
+              pos4 = header.indexOf('&');
+              redString = header.substring(pos1+1, pos2);
+              greenString = header.substring(pos2+1, pos3);
+              blueString = header.substring(pos3+1, pos4);
+              /*Serial.println(redString.toInt());
+              Serial.println(greenString.toInt());
+              Serial.println(blueString.toInt());*/
+              ledcWrite(redChannel, redString.toInt());
+              ledcWrite(greenChannel, greenString.toInt());
+              ledcWrite(blueChannel, blueString.toInt());
+            }
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}`,className:"w-code-text c1w06jcf ctqz4wr c1vdhz6d c6hmisz cvu7fmi c1o8a7nd cl7p029 c1jrsk1n c162scq1 czz4g3l csrfvpb c4c2ush"}),e.jsxs(r,{className:"w-paragraph ck3rk9r cz5s00m cgzpa1h",children:["After pasting this code make sure to replace the YOURSSID text and YOURPASSWORD text with the SSID of the wifi you","",e.jsx("br",{}),"","want to connect to and YOURPASSWORD with the password of the wifi you want to connect to, if you are struggling","",e.jsx("br",{}),"","to find these two in the code they are highlighted in red at the top of the code block in this page","",e.jsx("br",{}),"","",e.jsx("br",{}),"","After doing this you may proceed with the wiring for this part:"]}),e.jsx(c,{src:"/assets/Capture_d__cran_2024-12-06_105433_4oEcP_iTImmfTXFzdeHRv.png",width:815,height:712,className:"w-image cw8wk4g c1n8335o c1lpyau1 c1h1nl5c c13gmqhk c1x0pid4"}),e.jsx(c,{src:"/assets/IMG_20241205_161442215_2_Ceu_44MZzk6ae_DIx8bOE.jpg",width:3072,height:2682,className:"w-image cw8wk4g c1n8335o c1nkaght c1h1nl5c c13gmqhk ck4v81x"}),e.jsxs(r,{className:"w-paragraph ck3rk9r cz5s00m cgzpa1h",children:["If you plan on using the rgb led by yourself in the future keep in mind that the ","",e.jsx("br",{}),"","Red Green and Blue is ordered like shown below, you can easily tell which side red is by locating the longer pin"]}),e.jsx(c,{src:"/assets/Capture_d__cran_2024-12-06_105613_IJLfdgh2qF7hzuEoc4mip.png",width:611,height:155,className:"w-image cw8wk4g c1n8335o cex87fq c1h1nl5c c13gmqhk cqn6qth"}),e.jsx(r,{className:"w-paragraph ck3rk9r cz5s00m c1g05gyx",children:"After you finish the wiring  you can connect the board to you computer and upload the code."}),e.jsx(c,{src:"/assets/Capture_d__cran_2024-12-03_163950_ysuhPoNqTuJsdXaD1XHiu.png",width:459,height:71,className:"w-image cw8wk4g c1n8335o c1h1nl5c c13gmqhk"}),e.jsx(r,{className:"w-paragraph ck3rk9r cz5s00m c1g05gyx",children:"Once the code is uploaded​ start the serial monitor by pressing this button"}),e.jsx(c,{src:"/assets/imageee_cPTiUgVCkXSVT_sk4tbE5.png",width:197,height:107,className:"w-image cw8wk4g c1n8335o c1h1nl5c c13gmqhk"}),e.jsx(r,{className:"w-paragraph ck3rk9r cz5s00m c1g05gyx",children:"Make sure to select a baud rate of 115200"}),e.jsx(c,{src:"/assets/Capture_d__cran_2024-12-06_110857_ZhLLCbfQUx26s-wrEt_Ow.png",width:897,height:69,className:"w-image cw8wk4g c1n8335o c1h1nl5c c13gmqhk"}),e.jsx(r,{className:"w-paragraph ck3rk9r cz5s00m c1g05gyx",children:"Now press the physical reset button on the board"}),e.jsx(c,{src:"/assets/image10_LWg8UQakglldy6G1UjtkV.png",width:1920,height:1045,className:"w-image cw8wk4g c1n8335o cuow313 c1h1nl5c c13gmqhk c16shwhx"}),e.jsxs(r,{className:"w-paragraph ck3rk9r cz5s00m c1g05gyx",children:["we now have the local IP adress of the server, copy that into any recent web browser to acess it's website","",e.jsx("br",{}),"","",e.jsx("br",{}),"","If you did everything correctly this should display a webpage where you can remotely change the color of the RGB LED","",e.jsx("br",{}),"","If some colors display wrong makes sure you are using all three of the right resistors, that they are connected to the right place and that they aren't touching each other"]}),e.jsx(c,{src:"/assets/Capture_d__cran_2024-12-06_112355_R-UWixstSh9mrZhbB1slm.png",width:550,height:293,className:"w-image cw8wk4g c1n8335o cxtgk2x c1h1nl5c c13gmqhk c1tr23vj"})]}),e.jsxs(s,{className:"w-box",children:[e.jsx(h,{href:"/",className:"w-link c3j6eu6 cw8wk4g c1n8335o cnrqlv9 c1aj0wmm c5lna9m c19bzeoe c6gts8m cywnn7c cgpqfz5 c16mm8xg c8gwi5u c17dfjxw c1opx329 c1upkxol c1dukfjv c920vq8 c1drgsit co8ar9d c1d5e9u5 c1h1nl5c c13gmqhk c1hqpamb c1u5brzr cef8lky c1rvw9cl chewl0k cks4e9z cwqrhhf c1q5912w c1szy17v ci6o494 c17d547z c1dme089 ce6mn25",children:e.jsx(a,{tag:"span",className:"w-text",children:e.jsx(d,{className:"w-bold-text",children:"NEXT"})})}),e.jsx(h,{href:"/",className:"w-link c3j6eu6 cw8wk4g c1n8335o cnrqlv9 c1aj0wmm c5lna9m c19bzeoe c6gts8m c1u4q7z0 cgpqfz5 c16mm8xg c8gwi5u c17dfjxw c1opx329 c1upkxol c1dukfjv c920vq8 c1drgsit co8ar9d c1d5e9u5 c1h1nl5c c13gmqhk cghu1xd c1u5brzr cef8lky c1rvw9cl chewl0k cks4e9z cwqrhhf c1q5912w c1szy17v ci6o494 c17d547z c1dme089 ce6mn25",children:e.jsx(d,{className:"w-bold-text",children:"HOME"})})]})]}),R=({data:i})=>{const{pageMeta:t}=i,{origin:l}=new URL(i.url);let o=t.socialImageUrl;return t.socialImageAssetName&&(o=`${l}${m({src:t.socialImageAssetName,format:"raw"})}`),e.jsxs(e.Fragment,{children:[i.url&&e.jsx("meta",{property:"og:url",content:i.url}),e.jsx("title",{children:t.title}),e.jsx("meta",{property:"og:title",content:t.title}),t.description&&e.jsxs(e.Fragment,{children:[e.jsx("meta",{name:"description",content:t.description}),e.jsx("meta",{property:"og:description",content:t.description})]}),e.jsx("meta",{property:"og:type",content:"website"}),p,o&&e.jsx("meta",{property:"og:image",content:t.socialImageUrl}),p,t.excludePageFromSearch&&e.jsx("meta",{name:"robots",content:"noindex, nofollow"}),t.custom.map(({property:n,content:u})=>e.jsx("meta",{property:n,content:u},n)),T,C.map(n=>e.jsx("link",{rel:"preload",href:`${g}${n.name}`,as:"font",crossOrigin:"anonymous"},n.id)),I.map(n=>e.jsx("link",{rel:"preload",href:`${g}${n.name}`,as:"image"},n.id))]})},U=Object.freeze(Object.defineProperty({__proto__:null,Head:R},Symbol.toStringTag,{value:"Module"})),E=({data:i})=>{const{system:t,resources:l,url:o}=i;return e.jsx(z.Provider,{value:{imageLoader:m,assetBaseUrl:g,imageBaseUrl:N,resources:l},children:e.jsx(O,{system:t},o)})},L=Object.freeze(Object.defineProperty({__proto__:null,default:E},Symbol.toStringTag,{value:"Module"})),A={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"/renderer/+onRenderClient.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:P}},Head:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/copy-1/copy-1/+Head.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:U}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/copy-1/copy-1/+Page.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:L}}};export{A as configValuesSerialized};
