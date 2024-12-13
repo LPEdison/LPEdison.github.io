28b6cfad3226cdef4ca4d698bfc7b28369ec72e2
LPEdison <github.edison@gmx.com>
2024-12-13 16:25:44 +0100

    WebS 1.01

---
+++ assets/entries/pages_esp32_ir-alarm.CzTnp_BF.js
@@ -0,0 +1,186 @@
+import{j as e,f as y,a as c,b as d,p as t,O as w,x,n as f,M as j,R as k,N,S as _,s as i,$ as b,P as S,h as p,i as m,c as u,d as T,e as v,g as z}from"../chunks/chunk-D-jzwPvi.js";import{f as l}from"../chunks/chunk-CqHlRVf1.js";import{e as I}from"../chunks/chunk-CTFwEhyJ.js";import{l as P}from"../chunks/chunk-CWAJqpQR.js";/* empty css                      */const h=void 0,C=void 0,D=[],A=[],q=({})=>e.jsxs(y,{className:"w-body cef8lky c3i1u3x chk6qok",children:[e.jsxs(c,{className:"w-box cxit5en cogsgih ctdzyps ce19egy",children:[e.jsx(d,{href:"/",className:"w-link",children:e.jsx(t,{src:"/assets/Logo_fgtINaQGBFFapgXNvmtgD.png",width:405,height:451,className:"w-image c942crn c1uzvt68 cp3a1ni"})}),e.jsxs(w,{children:[e.jsx(x,{children:e.jsx(f,{className:"w-button c1ya628u c10ua9p1 c1qy06sr cpbg552 cshxx6g c1lub6ji c77fen c1kt3hjx c1geiwv ckcg2od cealztw cvs4hrp comp0x6 cqpuunw c1wvok5d c1eikf4o c1iwr2ut c1l088 c1fh3e63 c14zbtl7 c14xknmu c1mkwhsj c50kpvg c1tup317 c1lxdi4i c1xm82re caz9sw9 c3ke8ei c1vra1wj c1rkfrji c1t36fdn c1twcrqe",children:e.jsx(t,{src:"/assets/burger_H_E62dUjUdnjYrHNINiU_.png",width:100,height:100,className:"w-image"})})}),e.jsx(j,{className:"w-dialog-overlay c1n92csm c1e0yeox cpjqu7u csvz69a c62v901 cnpumd1 c15kyzwk c1yxak60 cbe9vyj cqsyioe c13jgw8t ccvhcq8",children:e.jsxs(k,{className:"w-dialog-content c1s06m5q cnpumd1 cbe9vyj cqsyioe c15zk8e1 cb947cc c10ury7n ckwfs5l c14zbtl7 c13gmqhk cpg64ub cxu11uu c6t5zc4 chqhe2i c1e23wal c12pfuk9 ci1jhro c1xyla5v",children:[e.jsxs(c,{tag:"nav",role:"navigation",className:"w-box",children:[e.jsxs(c,{className:"w-box cbe9vyj cqsyioe c1p8wosj c1wpxkon",children:[e.jsx(N,{className:"w-dialog-title c19otp7c c14dh9mz cmup7a7 cbyfgle cwvbj7b",children:"Sheet Title"}),e.jsx(_,{className:"w-dialog-description c19otp7c c14dh9mz c1geiwv ckcg2od c8wnu40",children:"Sheet description text you can edit"})]}),e.jsx(i,{className:"w-text",children:"The text you can edit"})]}),e.jsx(b,{className:"w-close-button c193n3ab c11gcw2s ci9xck c164arb c1mydgbc chzyyxd c1rkdclf cspx8cz cbe9vyj c1qy06sr cpbg552 c17x55us cpbtfu3 c1uqrecz c350vec cwwj380 ci4hcet c2ls53r cjhbkuo",children:e.jsx(S,{code:'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" width="100%" height="100%" style="display: block;"><path fill-rule="evenodd" d="M13.566 2.434a.8.8 0 0 1 0 1.132L9.13 8l4.435 4.434a.8.8 0 0 1-1.132 1.132L8 9.13l-4.434 4.435a.8.8 0 0 1-1.132-1.132L6.87 8 2.434 3.566a.8.8 0 0 1 1.132-1.132L8 6.87l4.434-4.435a.8.8 0 0 1 1.132 0Z" clip-rule="evenodd"/></svg>',className:"w-html-embed"})})]})})]})]}),e.jsxs(c,{className:"w-box ce19egy c1h1nl5c c13gmqhk cogsgih c16bzkdh c13ckunz cn13o9d c1tszs9s c1mmoi84 cw4dc5j c1933afg c1cl6xrl cex20e6",children:[e.jsxs(i,{className:"w-text chewl0k cs8jnol c1bb95j4 c192du2r c136d2hq",children:["Keep in mind these resistor colors",e.jsx(I,{href:"/esp32/ab/cd/ef/gh/ij/kl/mn/op/qr/secret",className:"w-rich-text-link cef8lky c1u5brzr c1pr4ejo",children:"!"})]}),e.jsxs(c,{className:"w-box c1h1nl5c c13gmqhk ckfm5zf c1f1okbg ch1fyv9 cwo00xu cotpzvh ccwx88h c11693zx co8eotk",children:[e.jsx(t,{src:"/assets/Capture_d__cran_2024-12-03_160109_PJ2UudRaJD8_bza_X_OKG.png",width:389,height:147,className:"w-image c1v19npp c13gmqhk c1q9b457 cgy3e4m"}),e.jsx(t,{src:"/assets/Capture_d__cran_2024-12-03_160212_eOl_PoBJ8HHeWHs-g4RPi.png",width:390,height:152,className:"w-image c1h1nl5c c19otp7c c14dh9mz c1st55tw c3793j1 c1uzvt68 cw8wk4g c1vcoo9v c3j6eu6 c1xui7l2"}),e.jsx(t,{src:"/assets/Capture_d__cran_2024-12-03_155524_dbw-G-XKY1jhwxOwFXklI.png",width:396,height:149,className:"w-image c1h1nl5c c13gmqhk c1uzvt68 c3793j1 c14zbtl7"})]})]}),e.jsxs(c,{className:"w-box",children:[e.jsx(i,{className:"w-text c1jsn5zb camnv7y c1j6gywh",children:e.jsx(i,{tag:"span",className:"w-text",children:e.jsx(p,{className:"w-bold-text c1c7894k",children:"IR Alarm"})})}),e.jsxs(l,{className:"w-paragraph ck3rk9r cz5s00m cgzpa1h c1g05gyx",children:["In this page we'll learn how to use an IR Sensor working with a buzzer to make a simple alarm system","",e.jsx("br",{}),"","",e.jsx("br",{}),"","Here is the wiring we will need, you don't have to follow it exactly but as long as everything connects to the right pins","",e.jsx("br",{}),"","you should be okay"]}),e.jsx(t,{src:"/assets/bread_alarm_ffb0nWvhiDIP6KfojagVA.png",width:1086,height:643,className:"w-image cw8wk4g c1n8335o czlur6q c1h1nl5c c13gmqhk c172k6fa"}),e.jsx(t,{src:"/assets/Capture_d__cran_2024-12-13_154327_fPOtnrVtYI5V_qek6SsUU.png",width:819,height:300,className:"w-image cw8wk4g c1n8335o czlur6q c1h1nl5c c13gmqhk c172k6fa c1y7eg9h cjp0mtu ce6iim8 c1mlptfz"}),e.jsx(l,{className:"w-paragraph ck3rk9r cz5s00m cgzpa1h c1g05gyx",children:"If needed feel free to adjust the cruciform knobs on the back of the IR sensor​"}),e.jsx(t,{src:"/assets/detect_kdSIFMD8ArmzpVf-OsuU8.png",width:2845,height:1881,className:"w-image cw8wk4g c1n8335o czlur6q c1h1nl5c c13gmqhk c172k6fa c1y7eg9h cjp0mtu ce6iim8 c1mlptfz"}),e.jsx(l,{className:"w-paragraph ck3rk9r cz5s00m cgzpa1h c1g05gyx",children:"Here is the code you need to send to the esp32"}),e.jsx(P,{code:`#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <WiFi.h>
+
+#define OLED_ADDRESS 0x3C
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define SCROLL_BUTTON_PIN 14 // Pin D23 for scrolling pages
+#define RESCAN_BUTTON_PIN 27 // Pin D15 for triggering rescan
+#define BUTTON_DEBOUNCE_DELAY 200 // Adjust this value as needed
+#define COUNTDOWN_DURATION 5000 // Countdown duration in milliseconds
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+int currentNetwork = 0;
+int numNetworks = 0;
+int currentPage = 1; // Initialize the page number to 1
+unsigned long lastButtonPressTime = 0;
+bool rescanRequested = false;
+unsigned long rescanStartTime = 0;
+
+void setup() {
+  Serial.begin(115200); // Initialize serial communication
+  pinMode(SCROLL_BUTTON_PIN, INPUT); // Set the scroll button pin as input with pull-up resistor
+  pinMode(RESCAN_BUTTON_PIN, INPUT); // Set the rescan button pin as input with pull-up resistor
+  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS)) {
+    Serial.println(F("Display failed to load."));
+    for (;;);
+  }
+
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.clearDisplay();
+  display.display();
+  displayIntroScreen(); // Display intro screen on startup
+  scanNetworks(); // Initial WiFi network scan
+}
+
+void loop() {
+  if (millis() - lastButtonPressTime > BUTTON_DEBOUNCE_DELAY) {
+    if (digitalRead(SCROLL_BUTTON_PIN) == HIGH) { // Check if the scroll button is pressed
+      lastButtonPressTime = millis(); // Record the time of button press
+      currentNetwork = (currentNetwork + 1) % numNetworks; // Move to the next network
+      currentPage = (currentPage % numNetworks) + 1; // Update the page number
+    }
+
+    if (digitalRead(RESCAN_BUTTON_PIN) == HIGH && !rescanRequested) { // Check if the rescan button is pressed and a rescan is not already requested
+      rescanRequested = true; // Set the flag to indicate rescan request
+      rescanStartTime = millis(); // Record the start time of rescan
+    }
+  }
+
+  if (rescanRequested) {
+    unsigned long elapsedTime = millis() - rescanStartTime;
+    if (elapsedTime < COUNTDOWN_DURATION) {
+      // Display countdown during the first COUNTDOWN_DURATION milliseconds
+      displayCountdown();
+    } else {
+      // Rescan networks after the countdown
+      scanNetworks(); // Perform the network rescan
+      rescanRequested = false; // Reset the rescan flag
+    }
+  } else {
+    // Display network information if rescan is not requested
+    displayNetworkInfo();
+  }
+}
+
+void displayIntroScreen() {
+  display.clearDisplay();
+  display.setTextSize(1); // Increased font size
+  display.setTextColor(SSD1306_WHITE);
+
+// Position the text
+  // Print bold text
+  display.setCursor(1, 1);
+  display.println("Esp-32 WIFI Scanner");
+  display.setCursor(1, 10);
+  display.println("");
+  display.setCursor(1, 20);
+  display.println("By A1D3N");
+
+  // Draw the text again with a slight offset
+  display.setCursor(2, 2);
+  display.println("Esp-32 WIFI Scanner");
+  display.setCursor(2, 11);
+  display.println("");
+  display.setCursor(2, 21);
+  display.println("By A1D3N");
+
+  // Position the ASCII cat
+  display.setCursor(0, 36); // Adjust Y position for the cat
+  display.println(" /\\\\_/\\\\");
+  display.println("( o.o )");
+  display.println(" > ^ <");
+  display.println("  ^ ^  ");
+
+
+  display.display();
+}
+
+
+void scanNetworks() {
+  Serial.println("Scanning networks..."); // Debug message
+  numNetworks = WiFi.scanNetworks();
+  currentNetwork = 0;
+  currentPage = 1;
+}
+
+void displayNetworkInfo() {
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.print("SSID: ");
+  display.println(WiFi.SSID(currentNetwork));
+  display.print("MAC:");
+  display.println(WiFi.BSSIDstr(currentNetwork));
+  display.print("Security: ");
+  display.println(getEncryptionTypeText(WiFi.encryptionType(currentNetwork)));
+  display.print("Channel: ");
+  display.println(WiFi.channel(currentNetwork));
+  display.print("RSSI: ");
+  display.println(WiFi.RSSI(currentNetwork));
+  // Estimate distance based on RSSI
+  double distance = estimateDistance(WiFi.RSSI(currentNetwork));
+  display.print("Distance: ");
+  display.print(distance);
+  display.println(" meters");
+ // Calculate the width of the page number text
+  int pageNumTextWidth = 6 * 2; // Assuming 2 digits for the page number
+  // Center-align the page number and shift four characters to the right
+  int pageNumX = (SCREEN_WIDTH - pageNumTextWidth) / 2 + 24; // Shift four characters to the right
+  display.setCursor(pageNumX, SCREEN_HEIGHT - 8);
+  display.print("Page ");
+  display.print(currentPage);
+  display.display();
+}
+
+
+void displayCountdown() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("Scanning networks...");
+  int secondsRemaining = (COUNTDOWN_DURATION - (millis() - rescanStartTime)) / 1000;
+  display.println("Time remaining: ");
+  display.print(secondsRemaining);
+  display.println(" seconds");
+  display.println("");
+  display.setCursor(38, 26); // Adjust Y position for the cat
+  display.println(" /\\\\_/\\\\");
+   display.setCursor(38, 36);
+  display.println("( o.o )");
+   display.setCursor(38, 46);
+  display.println(" > ^ <");
+   display.setCursor(38, 56);
+  display.println("  ^ ^  ");
+  display.display();
+}
+
+String getEncryptionTypeText(uint8_t encryptionType) {
+  switch (encryptionType) {
+    case WIFI_AUTH_OPEN:
+      return "None";
+    case WIFI_AUTH_WEP:
+      return "WEP";
+    case WIFI_AUTH_WPA_PSK:
+      return "WPA";
+    case WIFI_AUTH_WPA2_PSK:
+      return "WPA2";
+    case WIFI_AUTH_WPA_WPA2_PSK:
+      return "WPA/WPA2";
+    case WIFI_AUTH_WPA2_ENTERPRISE:
+      return "WPA2 Enterprise";
+    default:
+      return "Unknown";
+  }
+}
+
+double estimateDistance(int rssi) {
+  // Apply the path-loss model to estimate distance
+  const double A = -69; // Reference signal strength in dBm at 1 meter
+  const double n = 2.0; // Path-loss exponent, typically varies between 2 and 4
+  return pow(10, (A - rssi) / (10 * n));
+}`,lang:"C++",className:"w-code-text c1w06jcf ctqz4wr c1vdhz6d c6hmisz cvu7fmi c1o8a7nd cl7p029 c1jrsk1n c162scq1 czz4g3l csrfvpb c4c2ush"}),e.jsxs(l,{className:"w-paragraph ck3rk9r cz5s00m cgzpa1h c1g05gyx",children:["With this done you will have a very simple alarm system with a button that switches between home mode (green led) ","",e.jsx("br",{}),"","and alarm mode (red led)","",e.jsx("br",{}),"","",e.jsx("br",{}),"","In the next tutorial I'll go onto more detail on the code aspect so you can start making your own programs without just copy pasting,,, or you can just ignore everything and continue and copy pasteing anyways that's okay too"]})]}),e.jsxs(c,{className:"w-box",children:[e.jsx(d,{href:"/",className:"w-link c3j6eu6 cw8wk4g c1n8335o cnrqlv9 c1aj0wmm c5lna9m c19bzeoe c6gts8m cywnn7c cgpqfz5 c16mm8xg c8gwi5u c17dfjxw c1opx329 c1upkxol c1dukfjv c920vq8 c1drgsit co8ar9d c1d5e9u5 c1h1nl5c c13gmqhk c1hqpamb c1u5brzr cef8lky c1rvw9cl chewl0k cks4e9z cwqrhhf c1q5912w c1szy17v ci6o494 c17d547z c1dme089 ce6mn25",children:e.jsx(i,{tag:"span",className:"w-text",children:e.jsx(p,{className:"w-bold-text",children:"NEXT"})})}),e.jsx(d,{href:"/",className:"w-link c3j6eu6 cw8wk4g c1n8335o cnrqlv9 c1aj0wmm c5lna9m c19bzeoe c6gts8m c1u4q7z0 cgpqfz5 c16mm8xg c8gwi5u c17dfjxw c1opx329 c1upkxol c1dukfjv c920vq8 c1drgsit c16mmw3b c1d5e9u5 c1h1nl5c c13gmqhk cghu1xd c1u5brzr cef8lky c1rvw9cl chewl0k cks4e9z cwqrhhf c1q5912w c1szy17v ci6o494 c17d547z c1dme089 ce6mn25",children:e.jsx(p,{className:"w-bold-text",children:"HOME"})})]})]}),E=({data:a})=>{const{pageMeta:n}=a,{origin:o}=new URL(a.url);let r=n.socialImageUrl;return n.socialImageAssetName&&(r=`${o}${m({src:n.socialImageAssetName,format:"raw"})}`),e.jsxs(e.Fragment,{children:[a.url&&e.jsx("meta",{property:"og:url",content:a.url}),e.jsx("title",{children:n.title}),e.jsx("meta",{property:"og:title",content:n.title}),n.description&&e.jsxs(e.Fragment,{children:[e.jsx("meta",{name:"description",content:n.description}),e.jsx("meta",{property:"og:description",content:n.description})]}),e.jsx("meta",{property:"og:type",content:"website"}),h,r&&e.jsx("meta",{property:"og:image",content:n.socialImageUrl}),h,n.excludePageFromSearch&&e.jsx("meta",{name:"robots",content:"noindex, nofollow"}),n.custom.map(({property:s,content:g})=>e.jsx("meta",{property:s,content:g},s)),C,D.map(s=>e.jsx("link",{rel:"preload",href:`${u}${s.name}`,as:"font",crossOrigin:"anonymous"},s.id)),A.map(s=>e.jsx("link",{rel:"preload",href:`${u}${s.name}`,as:"image"},s.id))]})},R=Object.freeze(Object.defineProperty({__proto__:null,Head:E},Symbol.toStringTag,{value:"Module"})),U=({data:a})=>{const{system:n,resources:o,url:r}=a;return e.jsx(T.Provider,{value:{imageLoader:m,assetBaseUrl:u,imageBaseUrl:v,resources:o},children:e.jsx(q,{system:n},r)})},W=Object.freeze(Object.defineProperty({__proto__:null,default:U},Symbol.toStringTag,{value:"Module"})),M={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"/renderer/+onRenderClient.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:z}},Head:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/esp32/ir-alarm/+Head.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:R}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/esp32/ir-alarm/+Page.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:W}}};export{M as configValuesSerialized};
