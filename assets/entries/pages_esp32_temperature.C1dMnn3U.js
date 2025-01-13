e714352e6fe917e0dd5660514298d9c9c7ee035e
LPEdison <github.edison@gmx.com>
2025-01-13 10:25:18 +0100

    WebS 2.0

---
+++ assets/entries/pages_esp32_temperature.C1dMnn3U.js
@@ -0,0 +1,69 @@
+import{j as c,f as u,a as l,b as s,y as a,O as x,x as j,n as y,M as k,R as f,N as z,c as n,$ as v,P as b,s as d,i as w,d as h,e as q,g as N}from"../chunks/chunk-BobsVryZ.js";import{f as o,l as _}from"../chunks/chunk-DEzZCgSH.js";/* empty css                      */const p=void 0,S=void 0,T=[],D=[],H=({})=>c.jsxs(u,{className:"w-body cef8lky c3i1u3x cgzpa1h",children:[c.jsxs(l,{className:"w-box cxit5en cogsgih ctdzyps ce19egy",children:[c.jsx(s,{href:"/",className:"w-link",children:c.jsx(a,{src:"/assets/Logo_fgtINaQGBFFapgXNvmtgD.png",width:405,height:451,className:"w-image c942crn c1uzvt68 cp3a1ni"})}),c.jsxs(x,{children:[c.jsx(j,{children:c.jsx(y,{className:"w-button c1ya628u c10ua9p1 c1qy06sr cpbg552 cshxx6g c1lub6ji c77fen c1kt3hjx c1geiwv ckcg2od cealztw cvs4hrp comp0x6 cqpuunw c1wvok5d c1eikf4o c1iwr2ut c1l088 c1fh3e63 c14zbtl7 c14xknmu c1mkwhsj c50kpvg c1tup317 c1lxdi4i c1xm82re caz9sw9 c3ke8ei c1vra1wj c1rkfrji c1t36fdn c1twcrqe",children:c.jsx(a,{src:"/assets/burger_H_E62dUjUdnjYrHNINiU_.png",width:100,height:100,className:"w-image"})})}),c.jsx(k,{className:"w-dialog-overlay c1n92csm c1e0yeox cpjqu7u csvz69a c62v901 cnpumd1 c15kyzwk c1yxak60 cbe9vyj cqsyioe c13jgw8t ccvhcq8",children:c.jsxs(f,{className:"w-dialog-content c1s06m5q cnpumd1 cbe9vyj cqsyioe c15zk8e1 cb947cc c10ury7n ckwfs5l c14zbtl7 c13gmqhk cpg64ub cxu11uu c6t5zc4 chqhe2i c1e23wal c12pfuk9 ci1jhro c1xyla5v",children:[c.jsx(l,{tag:"nav",role:"navigation",className:"w-box",children:c.jsxs(l,{className:"w-box cbe9vyj cqsyioe c1p8wosj c1wpxkon",children:[c.jsx(z,{className:"w-dialog-title c19otp7c c1tpu3fb cmup7a7 c12hhjv2 cwvbj7b cmkc8rd",children:"ESP32"}),c.jsx(s,{href:"/esp32/getting-started",className:"w-link c3j6eu6 c1n8335o cnrqlv9 c1aj0wmm c5lna9m c19bzeoe c6gts8m cywnn7c cgpqfz5 c1wpmkg6 c8gwi5u c17dfjxw c1opx329 c1upkxol c1dukfjv c920vq8 cdhma3r ck50qr6 co8ar9d c1d5e9u5 c14ghf94 c1u5brzr cef8lky c1j915ev chewl0k cks4e9z cs1n5w0 c1q5912w c1szy17v ci6o494 c17d547z c1dme089 c13gmqhk c1gw1tt6",children:c.jsx(n,{className:"w-bold-text",children:"Getting Started"})}),c.jsx(s,{href:"/esp32/getting-started",className:"w-link c3j6eu6 c1n8335o cnrqlv9 c1aj0wmm c5lna9m c19bzeoe c6gts8m cywnn7c cgpqfz5 c1wpmkg6 c8gwi5u c17dfjxw c1opx329 c1upkxol c1dukfjv c920vq8 cdhma3r ck50qr6 co8ar9d c1d5e9u5 clhuzwz c1u5brzr cef8lky c1j915ev chewl0k cks4e9z cs1n5w0 c1q5912w c1szy17v ci6o494 c17d547z c1dme089 c13gmqhk c1gw1tt6",children:c.jsx(n,{className:"w-bold-text",children:"RGB Server"})}),c.jsx(s,{href:"/esp32/wifi-scanner",className:"w-link c3j6eu6 c1n8335o cnrqlv9 c1aj0wmm c5lna9m c19bzeoe c6gts8m cywnn7c cgpqfz5 c1wpmkg6 c8gwi5u c17dfjxw c1opx329 c1upkxol c1dukfjv c920vq8 cdhma3r ck50qr6 co8ar9d c1d5e9u5 clhuzwz c1u5brzr cef8lky c1j915ev chewl0k cks4e9z cs1n5w0 c1q5912w c1szy17v ci6o494 c17d547z c1dme089 c13gmqhk c1gw1tt6",children:c.jsx(n,{className:"w-bold-text",children:"WIFI Scanner"})}),c.jsx(s,{href:"/esp32/ir-alarm",className:"w-link c3j6eu6 c1n8335o cnrqlv9 c1aj0wmm c5lna9m c19bzeoe c6gts8m cywnn7c cgpqfz5 c1wpmkg6 c8gwi5u c17dfjxw c1opx329 c1upkxol c1dukfjv c920vq8 cdhma3r ck50qr6 co8ar9d c1d5e9u5 clhuzwz c1u5brzr cef8lky c1j915ev chewl0k cks4e9z cs1n5w0 c1q5912w c1szy17v ci6o494 c17d547z c1dme089 c13gmqhk c1gw1tt6",children:c.jsx(n,{className:"w-bold-text",children:"IR Alarm"})}),c.jsx(s,{href:"/esp32/potentiometer",className:"w-link c3j6eu6 c1n8335o cnrqlv9 c1aj0wmm c5lna9m c19bzeoe c6gts8m cywnn7c cgpqfz5 c1wpmkg6 c8gwi5u c17dfjxw c1opx329 c1upkxol c1dukfjv c920vq8 cdhma3r ck50qr6 co8ar9d c1d5e9u5 clhuzwz c1u5brzr cef8lky c1j915ev chewl0k cks4e9z cs1n5w0 c1q5912w c1szy17v ci6o494 c17d547z c1dme089 c13gmqhk c1gw1tt6",children:c.jsx(n,{className:"w-bold-text",children:"Potentiometer"})})]})}),c.jsx(v,{className:"w-close-button c193n3ab c11gcw2s ci9xck c164arb c1mydgbc chzyyxd c1rkdclf cspx8cz cbe9vyj c1qy06sr cpbg552 c17x55us cpbtfu3 c1uqrecz c350vec cwwj380 ci4hcet c2ls53r cjhbkuo",children:c.jsx(b,{code:'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" width="100%" height="100%" style="display: block;"><path fill-rule="evenodd" d="M13.566 2.434a.8.8 0 0 1 0 1.132L9.13 8l4.435 4.434a.8.8 0 0 1-1.132 1.132L8 9.13l-4.434 4.435a.8.8 0 0 1-1.132-1.132L6.87 8 2.434 3.566a.8.8 0 0 1 1.132-1.132L8 6.87l4.434-4.435a.8.8 0 0 1 1.132 0Z" clip-rule="evenodd"/></svg>',className:"w-html-embed"})})]})})]})]}),c.jsxs(l,{className:"w-box ce19egy c1h1nl5c c13gmqhk cogsgih c16bzkdh c13ckunz cn13o9d c1tszs9s c1mmoi84 cw4dc5j c1933afg c1cl6xrl cex20e6",children:[c.jsxs(d,{className:"w-text chewl0k cs8jnol c1d73b5o c192du2r c136d2hq cnykfat",children:["Keep in mind these resistor colors",c.jsx(s,{className:"w-rich-text-link cef8lky c1u5brzr c1pr4ejo",children:"!"})]}),c.jsxs(l,{className:"w-box c1h1nl5c c13gmqhk ckfm5zf c1f1okbg ch1fyv9 cwo00xu cotpzvh ccwx88h c11693zx co8eotk",children:[c.jsx(a,{src:"/assets/Capture_d__cran_2024-12-03_160109_PJ2UudRaJD8_bza_X_OKG.png",width:389,height:147,className:"w-image c1v19npp c13gmqhk c1q9b457 cgy3e4m"}),c.jsx(a,{src:"/assets/Capture_d__cran_2024-12-03_160212_eOl_PoBJ8HHeWHs-g4RPi.png",width:390,height:152,className:"w-image c1h1nl5c c19otp7c c14dh9mz c1st55tw c3793j1 c1uzvt68 cw8wk4g c1vcoo9v c3j6eu6 c1xui7l2"}),c.jsx(a,{src:"/assets/Capture_d__cran_2024-12-03_155524_dbw-G-XKY1jhwxOwFXklI.png",width:396,height:149,className:"w-image c1h1nl5c c13gmqhk c1uzvt68 c3793j1 c14zbtl7"})]})]}),c.jsxs(l,{className:"w-box",children:[c.jsx(d,{className:"w-text c1jsn5zb camnv7y c1j6gywh",children:c.jsx(d,{tag:"span",className:"w-text",children:c.jsx(n,{className:"w-bold-text c1c7894k",children:"Temperature and Humidity"})})}),c.jsxs(o,{className:"w-paragraph ck3rk9r cz5s00m cgzpa1h c1g05gyx",children:["In this page we'll learn how to use a DHT11 with an OLED screen like we used for the wifi scanner to make run a simple program showing the current temperature and humidity level","",c.jsx("br",{}),"","",c.jsx("br",{}),"","Here is the wiring we will need, you don't have to follow it exactly but as long as everything connects to the right pins","",c.jsx("br",{}),"","you should be okay"]}),c.jsx(a,{src:"/assets/dht11_hoFUVwTPUkRJp3JkwIRQ4.png",width:625,height:535,className:"w-image cw8wk4g c1n8335o czlur6q c1h1nl5c c13gmqhk c172k6fa c1wglhip c6odoxx c1r0q7mq c4avjvj"}),c.jsx(a,{src:"/assets/IMG_20241217_083838410_YMswf-wVTuL6dM48Bzq7U.jpg",width:4080,height:3072,className:"w-image cw8wk4g c1n8335o czlur6q c1h1nl5c c13gmqhk c172k6fa c1y7eg9h cjp0mtu ce6iim8 c1mlptfz"}),c.jsx(o,{className:"w-paragraph ck3rk9r cz5s00m cgzpa1h c1g05gyx",children:"you will also need to install two adafruit DHT sensor library  and the dht unified sensor library in order to make this code work"}),c.jsx(a,{src:"/assets/Capture_d__cran_2024-12-17_084451_i7H7Bne4BW8w3LhaWL2zD.png",width:357,height:609,className:"w-image cw8wk4g c1n8335o czlur6q c1h1nl5c c13gmqhk cn734r9 c1y7eg9h cjp0mtu ce6iim8 c1mlptfz"}),c.jsx(a,{src:"/assets/Capture_d__cran_2024-12-17_084932_90o_blKtWm6-opVy8A8nM.png",width:286,height:383,className:"w-image cw8wk4g c1n8335o czlur6q c1h1nl5c c13gmqhk cn734r9 c1y7eg9h cjp0mtu ce6iim8 c1mlptfz"}),c.jsx(o,{className:"w-paragraph ck3rk9r cz5s00m cgzpa1h c1g05gyx",children:"Here is the code you need to send to the esp32 once this is done"}),c.jsx(_,{code:`#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+#define DHTPIN 4     // Digital pin connected to the DHT sensor
+
+#define DHTTYPE    DHT11    // DHT 22 (AM2302)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+
+  dht.begin();
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+}
+
+void loop() {
+  delay(5000);
+
+  //read temperature and humidity
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+  }
+  // clear display
+  display.clearDisplay();
+
+  // display temperature
+  display.setTextSize(1);
+  display.setCursor(0,0);
+  display.print("Temperature: ");
+  display.setTextSize(2);
+  display.setCursor(0,10);
+  display.print(t);
+  display.print(" ");
+  display.setTextSize(1);
+  display.cp437(true);
+  display.write(167);
+  display.setTextSize(2);
+  display.print("C");
+
+  // display humidity
+  display.setTextSize(1);
+  display.setCursor(0, 35);
+  display.print("Humidity: ");
+  display.setTextSize(2);
+  display.setCursor(0, 45);
+  display.print(h);
+  display.print(" %"); 
+
+  display.display(); 
+}`,lang:"C++",className:"w-code-text c1w06jcf ctqz4wr c1vdhz6d c6hmisz cvu7fmi c18f5rll cl7p029 c1jrsk1n c162scq1 czz4g3l csrfvpb c4c2ush"}),c.jsx(o,{className:"w-paragraph ck3rk9r cz5s00m cgzpa1h c1g05gyx",children:"With this done you will have simple oled screen which displays the temperature and the current humidity in the room!"})]}),c.jsxs(l,{className:"w-box",children:[c.jsx(s,{href:"/esp32/potentiometer",className:"w-link c3j6eu6 cw8wk4g c1n8335o cnrqlv9 c1aj0wmm c5lna9m c19bzeoe c6gts8m cywnn7c cgpqfz5 c16mm8xg c8gwi5u c17dfjxw c1opx329 c1upkxol c1dukfjv c920vq8 c1drgsit co8ar9d c1d5e9u5 c1h1nl5c c13gmqhk c1hqpamb c1u5brzr cef8lky c1rvw9cl chewl0k cks4e9z cwqrhhf c1q5912w c1szy17v ci6o494 c17d547z c1dme089 ce6mn25",children:c.jsx(d,{tag:"span",className:"w-text",children:c.jsx(n,{className:"w-bold-text",children:"NEXT"})})}),c.jsx(s,{href:"/",className:"w-link c3j6eu6 cw8wk4g c1n8335o cnrqlv9 c1aj0wmm c5lna9m c19bzeoe c6gts8m c1u4q7z0 cgpqfz5 c16mm8xg c8gwi5u c17dfjxw c1opx329 c1upkxol c1dukfjv c920vq8 c1drgsit c16mmw3b c1d5e9u5 c1h1nl5c c13gmqhk cghu1xd c1u5brzr cef8lky c1rvw9cl chewl0k cks4e9z cwqrhhf c1q5912w c1szy17v ci6o494 c17d547z c1dme089 ce6mn25",children:c.jsx(n,{className:"w-bold-text",children:"HOME"})})]})]}),C=({data:r})=>{const{pageMeta:e}=r,{origin:m}=new URL(r.url);let i=e.socialImageUrl;return e.socialImageAssetName&&(i=`${m}${w({src:e.socialImageAssetName,format:"raw"})}`),c.jsxs(c.Fragment,{children:[r.url&&c.jsx("meta",{property:"og:url",content:r.url}),c.jsx("title",{children:e.title}),c.jsx("meta",{property:"og:title",content:e.title}),e.description&&c.jsxs(c.Fragment,{children:[c.jsx("meta",{name:"description",content:e.description}),c.jsx("meta",{property:"og:description",content:e.description})]}),c.jsx("meta",{property:"og:type",content:"website"}),p,i&&c.jsx("meta",{property:"og:image",content:e.socialImageUrl}),p,e.excludePageFromSearch&&c.jsx("meta",{name:"robots",content:"noindex, nofollow"}),e.custom.map(({property:t,content:g})=>c.jsx("meta",{property:t,content:g},t)),S,T.map(t=>c.jsx("link",{rel:"preload",href:`${h}${t.name}`,as:"font",crossOrigin:"anonymous"},t.id)),D.map(t=>c.jsx("link",{rel:"preload",href:`${h}${t.name}`,as:"image"},t.id))]})},E=Object.freeze(Object.defineProperty({__proto__:null,Head:C},Symbol.toStringTag,{value:"Module"})),P=({data:r})=>{const{system:e,resources:m,url:i}=r;return c.jsx(q.Provider,{value:{imageLoader:w,assetBaseUrl:h,resources:m},children:c.jsx(H,{system:e},i)})},I=Object.freeze(Object.defineProperty({__proto__:null,default:P},Symbol.toStringTag,{value:"Module"})),L={isClientRuntimeLoaded:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:!0}},onBeforeRenderEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:null}},dataEnv:{type:"computed",definedAtData:null,valueSerialized:{type:"js-serialized",value:{server:!0}}},onRenderClient:{type:"standard",definedAtData:{filePathToShowToUser:"/renderer/+onRenderClient.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:N}},Head:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/esp32/temperature/+Head.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:E}},Page:{type:"standard",definedAtData:{filePathToShowToUser:"/pages/esp32/temperature/+Page.tsx",fileExportPathToShowToUser:[]},valueSerialized:{type:"plus-file",exportValues:I}}};export{L as configValuesSerialized};
